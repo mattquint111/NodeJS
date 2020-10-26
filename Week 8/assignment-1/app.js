54f3ef16bcd8782ff2f1df1d2f74d7d9cb3d80a0
Matthew Quint <mattquint111@gmail.com>
2020-10-26 17:05:36 -0500

    assignment 1

--- Week 8/assignment-1/app.js
+++ Week 8/assignment-1/app.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
 const indexRouter = require('./routes/index')
+const loginRouter = require('./routes/login')
+const session = require('express-session')
 
 app.use(express.urlencoded())
 app.use('/', indexRouter)
+app.use('/login', loginRouter+)
 app.use('/css', express.static('css'))
+app.use(session({
+    secret: 'grumpy cat',
+    resave: false,
+    saveUninitialized: true
+}))
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
